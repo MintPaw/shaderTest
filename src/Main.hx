bfd337354542ff66c363f01267ee2288019216d1
MintPaw <jerusanders@gmail.com>
2015-12-18 15:47:07 -0700

    Flixel init

---
+++ src/Main.hx
@@ -0,0 +1,16 @@
+package;
+
+import openfl.display.Sprite;
+import flixel.FlxGame;
+
+class Main extends Sprite
+{
+
+	public function new()
+	{
+		super();
+
+		var flixel:FlxGame = new FlxGame(1280, 720, MainState, 1, 60, 60, true);
+		addChild(flixel);
+	}
+}
