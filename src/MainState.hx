88921523dc1f9c37bd5565b1861cf2598963edb0
MintPaw <jerusanders@gmail.com>
2016-02-12 15:58:12 -0700

    arc

--- src/MainState.hx
+++ src/MainState.hx
@@ -1,12 +1,28 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.effects.postprocess.PostProcess;
 
 class MainState extends FlxState
 {
-
+	private var _shader:PostProcess = new PostProcess("assets/shader/test.frag");
+	private var _frameCount:Int = 0;
+	
 	public function new()
 	{
 		super();
+	}
+
+	override public function create():Void
+	{
+		FlxG.addPostProcess(_shader);
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+		_frameCount++;
+		//_shader.setUniform("time", _frameCount*1000);
 	}
 }
